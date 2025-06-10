af2bccac4d9b78d8b1091fe9e3a7aa34337666bf
axryap27 <aarya27@gmail.com>
2025-06-10 14:17:48 -0500

    initial project files

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
-// hello this is main.c
+/*main.c*/
+
+//
+// Loops indefinitely and takes user input to output 
+// various statistics on DIVVY bikes 
 //
 // Author:
 // Aarya Patel
@@ -7,3 +11,33 @@
 // DIVVY Data analysis
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dist.h"
+
+
+// station struct
+struct Station {
+    char* stationID;
+    int capacity;
+    double latitude;
+    double longitude;
+    char* name;
+};
+
+
+// trip struct
+struct Trip {
+    char* tripID;
+    char* bikeID;
+    char* startStationID;
+    char* endStationID;
+    int duration;
+    char* startTime;
+};
+
+
+int main(){
+    
+}
